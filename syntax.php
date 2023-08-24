4cdd46c203aa93c3c0113010b4f4b096234aa432
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-24 12:09:09 +0100

    Create syntax.php

---
+++ syntax.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syntax</title>
+</head>
+<body>
+    <!-- using variables in php -->
+
+    <?php
+        $name = 'denzel';
+        $age = 19;
+        $isMale = true;
+        $height = 1.83;
+        $salary = null;
+
+        echo $name.'<br>';
+        echo $height.'<br>';  
+        echo $isMale.'<br>';
+        echo $age.'<br>';
+        echo $salary.'<br>';
+     ?>
+</body> 
+</html>
