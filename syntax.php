18581a4d4305ad9d4f4bafb871f504b33d54e010
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-24 12:23:59 +0100

    getting used to the syntax

--- syntax.php
+++ syntax.php
@@ -19,8 +19,21 @@
         echo $height.'<br>';  
         echo $isMale.'<br>';
         echo $age.'<br>';
-        echo $salary.;
+        //no output seen
+        echo $salary;
 
+        echo '<br>';
+        //getting the variable's data type
+        echo gettype($name).'<br>';
+        echo gettype($height).'<br>';
+        echo gettype($isMale).'<br>';
+        echo gettype($age).'<br>';
+        echo gettype($salary).'<br>';
+
+        //using var_dump shows the data type, its length and the value of the variable
+        var_dump($name);
+        echo '<br>';
+        var_dump($salary);
      ?>
 </body> 
 </html>
