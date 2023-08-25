8aa9ddfcd4ebc985695be098b147e5cea3565713
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-25 15:21:08 +0100

    creating a directory

--- file system/index.php
+++ file system/index.php
@@ -1,3 +1,13 @@
 <?php
+//    working  with magical constants 
 
+    //prints the current directory of the file
+    echo __DIR__.'<br>';
+    //prints the directory of the file being used 
+    echo __FILE__.'<br>';
+    //prints the line in which "LINE" is being written i.e 7
+    echo __LINE__.'<br>';
+
+    // creating directory
+     mkdir('test');
 ?>
