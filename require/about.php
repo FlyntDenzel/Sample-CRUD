e4f2eb30e230b57adba5cbee6a909def01169441
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-25 15:02:24 +0100

    created a new header file for storing headers and included it

--- require/about.php
+++ require/about.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <a href="index.php">Home</a>
-        <a href="about.php">Home</a>
-    </header>    
-  
-
+  <?php
+        include "header.php";
+  ?>  
     <h3>Nothing fun here to see </h3>
+
+    <footer>
+        Coyright 2023 Flynt
+    </footer>
 </body>
 </html>
--- require/index.php
+++ require/index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <a href="index.php">Home</a>
-        <a href="about.php">Home</a>
-    </header>    
+<?php
+    include "header.php";
+?>  
 
     <h3>Welcome to my first php page</h3>
     <h3>I make use of include and require here</h3>
+
+    <footer>
+        Coyright 2023 Flynt
+    </footer>
 </body>
 </html>
