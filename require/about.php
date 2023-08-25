1ba861fbb839206174acec295688b5c414aa1a78
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-25 14:53:46 +0100

    linked the two pages

---
+++ require/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <a href="index.php">Home</a>
+        <a href="about.php">Home</a>
+    </header>    
+  
+
+    <h3>Nothing fun here to see </h3>
+</body>
+</html>
