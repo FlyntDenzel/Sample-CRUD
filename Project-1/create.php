e2f40e74c416ae3cf93c83b7cff732a5f11d3f3f
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-26 10:07:25 +0100

    created a new product with post method using exec

--- Project-1/create.php
+++ Project-1/create.php
@@ -1,3 +1,20 @@
+<?php 
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $date = date('Y-m-d H:i:s');
+
+    //inserting elements into the table name i.e products
+    $pdo -> exec("INSERT INTO products (title, image, description, price, create_date)
+                VALUE ('$title', '', '$description', $price, '$date')
+
+    ");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,23 +27,25 @@
     
     <div class="container">
         <h1 class="my-3">Create New Product</h1>
-         <form>
+         <form action="create.php" method="post">
                 <div class="form-group">
                     <label>Product Image</label>
-                    <input type="file">
+                    <br>
+                    <input type="file" name="image">
                 </div> 
                 <div class="form-group">
                     <label>Product Title</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" name="title">
                 </div>
                 <div class="form-group">
                     <label>Product Description</label>
-                    <textarea  class="form-control"></textarea>
+                    <textarea  class="form-control" name="description"></textarea>
                 </div>
                 <div class="form-group">
                     <label>Product Price</label>
-                    <input type="number" step="0.01" class="form-control">
+                    <input type="number" step="0.01"  name="price" class="form-control">
                 </div>
+
         <button type="submit" class="btn btn-primary my-3">Submit</button>
     </form>
     </div>
