d146320c87efc75f53152f4019e12d9f8232f11c
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-26 12:32:52 +0100

    created user form

--- Project-2/user.php
+++ Project-2/user.php
@@ -4,8 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
 </head>
 <body>
-    
+    <div class="container">
+        <form method="post" class="my-3">
+             <div class="form-group my-3">
+                <label for="email">Name</label>
+                <input type="text" class="form-control my-2" placeholder="enter your name here">
+            </div>
+            <div class="form-group my-3">
+                <label for="email">Email</label>
+                <input type="text" class="form-control my-2" placeholder="enter your email here">
+            </div>
+            <div class="form-group my-3">
+                <label for="email">Phone Number</label>
+                <input type="text" class="form-control my-2" placeholder="enter your phone number here">
+            </div>
+            <div class="form-group my-3">
+                <label for="password">Password</label>
+                <input type="password" class="form-control my-2" placeholder="enter your password here">
+            </div>
+            <a href="" class="btn btn-primary my-3">Submit</a>
+        </form>
+    </div>
+
 </body>
 </html>
