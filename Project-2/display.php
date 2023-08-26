fb7a19378d9e2b7314a805f904c259e2fc8df4f0
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-08-26 13:52:22 +0100

    created display file

---
+++ Project-2/display.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Display</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <button class="btn btn-primary my-4">Add User</button>
+    </div>
+
+</body>
+</html>
